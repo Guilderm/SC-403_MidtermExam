bd1089ccb5463f814a08573fd048e2641011d81d
Guilder W. Milliner <guilderw@gmail.com>
2022-03-09 20:56:48 -0600

    Prior ot submiting

--- src/main/java/dev/guilder/midtermexam/Controller/publicPages.java
+++ src/main/java/dev/guilder/midtermexam/Controller/publicPages.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class publicPages {
-    @GetMapping("/")
+    @GetMapping({"/","/home","/homePage"})
     public String HomePage() {
         return "publicPages/homePage";
     }
 
-    @GetMapping("/aboutPage")
+    @GetMapping({"/about","/aboutPage"})
     public String AboutPage() {
         return "publicPages/aboutPage";
     }
